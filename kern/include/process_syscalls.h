de6fee57548c9bdd5d978e1a32b19440ad983117
Andrew Osthoff <ostho005@umn.edu>
2017-03-20 00:27:29 -0500

    Some skeleton work/todos about fork

--- kern/include/process_syscalls.h
+++ kern/include/process_syscalls.h
@@ -2,10 +2,11 @@
 #define PROCESS_SYSCALLS_H_
 
 #include <types.h>
+#include <mips/trapframe.h>
 
 pid_t sys_getpid(void);
 //not implemented yet, these are the signatures from userland/include/unistd.h 
-pid_t sys_fork(void);
+pid_t sys_fork(struct trapframe* tf, int32_t* retval);
 // pid_t waitpid(pid_t pid, int *returncode, int flags);
 // int execv(const char *prog, char *const *args);
 //__DEAD void sys_exit(int code);
--- kern/syscall/process_syscalls.c
+++ kern/syscall/process_syscalls.c
@@ -7,6 +7,7 @@
 #include <proc.h> 
 #include <current.h> // to get curthread
 #include <kern/limits.h> //to get the __PID_MIN and __PID_MAX macros
+#include <thread.h> //to get definition of thread struct
 
 //defined in machine-dependent types.h, evals to signed 32-bit int for MIPS
 
@@ -16,7 +17,29 @@ pid_t sys_getpid(void) {
   return curthread->t_proc->pid;
 }
 
-pid_t sys_fork() {
-  kprintf("Forked.\n");
-  return 1;
+pid_t sys_fork(struct trapframe* tf, int32_t* retval) {
+  //arguments only here for now to prevent warning
+  (void)*retval;
+  //struct thread* child_thread;
+
+  struct trapframe* child_tf = kmalloc(sizeof(struct trapframe));
+  if(child_tf == NULL) {
+    *retval = -1;
+    return ENOMEM;
+  }
+  *child_tf = *tf; 
+  //TODO: As far as I can tell we *have no* file system...so I don't
+  // know if there's no address space needed or what exactly
+
+  /*TODO: finish all of this...I honestly can't tell whether from here in 
+   this should do everything by hand and then call thread_fork(); or call 
+   enter_forked_process() and then have that call mips_usermode() 
+   (which is in arch/mips/locore/trap.c and mentions doing this). 
+   I think either would be fine...and I don't know how either would ultimately work
+
+   Either way, have to make sure the return value is consistant with posix/the man pages, 
+   but *retval is set to be the child pid for the parent, and 0 within the child.
+  */ 
+
+  return 0;
 }
